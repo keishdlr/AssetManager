b4663f279c866b601be8d9069abc1c9ae2399c05
keisha De La Rosa <keishdlr@gmail.com>
2025-10-28 15:37:52 -0400

    classes have constructors, getters, and setters

--- src/main/java/com/pluralsight/House.java
+++ src/main/java/com/pluralsight/House.java
@@ -1,4 +1,60 @@
 package com.pluralsight;
 
-public class House {
+public class House extends Asset{
+
+    String address;
+    int condition;
+    int squareFoot;
+    int lotSize;
+
+    public House(String description, String dateAcquired, double originalCost, String address, int condition, int squareFoot, int lotSize) {
+        super(description, dateAcquired, originalCost);
+        this.address = address;
+        this.condition = condition;
+        this.squareFoot = squareFoot;
+        this.lotSize = lotSize;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCondition() {
+        return condition;
+    }
+
+    public void setCondition(int condition) {
+        this.condition = condition;
+    }
+
+    public int getSquareFoot() {
+        return squareFoot;
+    }
+
+    public void setSquareFoot(int squareFoot) {
+        this.squareFoot = squareFoot;
+    }
+
+    public int getLotSize() {
+        return lotSize;
+    }
+
+    public void setLotSize(int lotSize) {
+        this.lotSize = lotSize;
+    }
+
+    @Override
+    public double getValue(){
+        // A house's value is determined as
+        // $180.00 per square foot (excellent)
+        // $130.00 per square foot (good)
+        // $90.00 per square foot (fair)
+        // $80.00 per square foot (poor)
+        // PLUS  25 cents per square foot of lot size
+
+    }
 }
